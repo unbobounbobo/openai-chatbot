f9eda134b454a2ef60168ac472293f491e3c2acf
unbobounbobo <59304729+unbobounbobo@users.noreply.github.com>
2023-10-16 00:34:22 +0000

    3.5に変更

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.title("ChatGPTのWEBアプリテストです")
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4"
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -15,7 +15,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("なにかお手伝いできますか?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
