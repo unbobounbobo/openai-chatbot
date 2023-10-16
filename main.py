8c5ee7d5e6bbbd5a3765dae84dd0d8b975cf6bf4
unbobounbobo <59304729+unbobounbobo@users.noreply.github.com>
2023-10-16 00:29:38 +0000

    gpt4に変更

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import openai
 
-st.title("ChatGPT-like clone")
+st.title("ChatGPTのWEBアプリテストです")
 
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
